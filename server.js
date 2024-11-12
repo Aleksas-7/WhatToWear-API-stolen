93ca406369f83ddcd24546dabaeb9a92cdc757ad
mykolassl <emykolas@gmail.com>
2024-11-12 19:47:36 +0200

    fix cors

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+        credentials: true,
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
